8ee250bacc45e417b756ac5c7f2d16a66c4caf70
Srilatha <koduri0311@gmail.com>
2024-11-27 20:23:29 +0000

    shell

--- shell/01.date-time.sh
+++ shell/01.date-time.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+echo "Hello, $LOGNAME"
 echo "Today's date is 'date'"
 echo "Username is 'username'"
 echo "Current directory is 'pwd'"
+
+# #!/bin/sh
+# echo "Hello, $LOGNAME"
+# echo "Today's date is `date`"
+# echo "Username is `who i am`"
+# echo "Current directory is `pwd`"
+
